7bc2a27e829c3d92552bebadb89251a92da82061
Maxime <maximemassing.pro@gmail.com>
2020-06-23 02:20:36 +0200

    Page Compte V1 : modif mdp et email ok

--- pages/compte.php
+++ pages/compte.php
@@ -1,12 +1,75 @@
-<?php require 'header.php'; 
+<?php require 'header_compte.php'; 
 
 if(!isset($_SESSION['auth'])){
     $_SESSION['danger'] = "Vous devez vous connecter pour accéder à cet page"; 
     header('Location: connexion.php'); 
     exit(); 
 }
+
+//si la variable post est rempli, ça veut dire qu'il a renvoyé soit le mdp soit l'email
+if(!empty($_POST)){
+
+    require_once '../inc/db.php'; 
+
+    if(!empty($_POST['email'])){
+        
+        $user_id = $_SESSION['auth']->id;
+        $req = $pdo->prepare('UPDATE users SET email = ? WHERE id = ? ');
+        $req->execute(array($_POST['email'], $user_id));
+
+        $req = $pdo->prepare('SELECT * FROM users WHERE email = ?'); 
+        $req->execute(array($_POST['email'])); 
+        $user = $req->fetch(); 
+        unset($_SESSION['auth']);
+        $_SESSION['auth'] = $user; 
+    }
+
+    if(!empty($_POST['mdp'])){
+
+        $user_id = $_SESSION['auth']->id;
+        $mdp_hash = password_hash($_POST['mdp'], PASSWORD_BCRYPT);
+        $req = $pdo->prepare('UPDATE users SET motdepasse = ? WHERE id = ? ');
+        $req->execute(array($mdp_hash, $user_id));
+
+    }
+
+}
+
 ?>
 
 <h1>Mon compte</h1>
+
+<div class ="bloc-bas">   
+                <div class ="informations">
+                    <div class="menu-information">
+                       <ul class="menu-vertical">
+                           <li class="info-perso">Informations Personnelles</li>
+                           <li class="li-histo">Historique de commande</li>
+                       </ul> 
+                    </div>
+
+                    <div class="mail">
+                        <form method ="POST" action="" class ="f-mail">
+                            <h3>E-mail</h3>
+                            <p><?php echo $_SESSION['auth']->email ?></p>
+                            <input type="email" name="email" id="email"/>
+                            <button type="submit" class = "modifier"> Modifier</button>
+                        </form>
+                    </div>
+
+                    <div class="mot-de-passe">
+                        <form method ="POST" action="" class ="f-mot-de-passe">
+                            <h3>Mot de passe</h3>
+                            <input type="password" name="mdp" id="mdp"/>
+                            <button type="submit" class = "modifier">Modifier</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    
+        
 
 <?php require 'footer.php'; ?>
---
+++ pages/header_compte.php
@@ -0,0 +1,46 @@
+<?php require 'header.php'; 
+
+echo "<link rel='stylesheet' type='text/css' href='../css/compte.css'>";
+?>
+
+
+
+<div id = "corps-de-page" class = "corps-de-page">
+            <div class = "bloc-navbar">
+                <div class ="navbar">
+                    <div class ="option" id ="tableau">
+                        <a href = "tableau-de-bord.html" class ="a-option" id="a-tableau">Tableau de bord</a>
+                    </div>
+                    <div class ="option" id ="Inventaire">
+                        <a href = "client-connecte.html" class ="a-option" id="a-Inventaire">Inventaire</a>
+                    </div>
+                    <div class ="option" id ="Service">
+                        <a href = "service-support.html" class ="a-option" id="a-Service">Service client</a>
+                    </div>
+                    <div class ="option" id ="Commentaires">
+                        <a href = "commentaires.html" class ="a-option" id="a-Commentaires">Commentaires</a>
+                    </div>
+                    <div class ="option" id ="offres">
+                        <a href = "#" class ="a-option" id="a-offres">Mes offres</a>
+                    </div>
+                    <div class ="option" id ="affiliation">
+                        <a href = "#" class ="a-option" id="a-affiliation">Programme d'affiliation</a>
+                    </div>
+                    <div class ="option" id ="Compte">
+                        <a href = "compte.html" class ="a-option" id="a-Compte">Compte</a>
+                    </div>
+                </div>
+            </div>
+            <div class = "bloc-navbar">
+                <div class ="navbar" id="bloc-navbar-compte">
+                    <div class ="option" id="achats">
+                        <a href = "compte.html" class ="a-option" id="a-achats">MES ACHATS</a>
+                    </div>
+                    <div class ="option" id="operations">
+                        <a href = "operations.html" class ="a-option" id="a-operations">OPERATIONS</a>
+                    </div>
+                    <div class ="option" id="reglages">
+                        <a href = "detail-compte.html" class ="a-option" id="a-reglages">DETAILS DU COMPTE</a>
+                    </div>
+                </div>
+            </div>
