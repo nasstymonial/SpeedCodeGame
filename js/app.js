5e40ae95a658bb01525838e6feb92d52eb6e2789
Nassim <nassimkissi@icloud.com>
2020-07-02 23:15:14 +0200

    suppression d'une fonction js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-(function($){
-
-    $('#addPanier').click(function(event){
-        event.preventDefault(); 
-        $.get($(this).attr('href'),{}, function(data){
-            if(data.error){
-                alert(data.message); 
-            }else{
-                if(confirm(data.message + '. Voulez vous consulter votre panier ?')){
-                    location.href = 'panier.php'; 
-                } else{
-                    $('#count').empty().append(data.count);
-                }
-
-            }
-        },'json');
-        return false;
-    }); 
-})(jQuery); 
-
 $(document).ready(function(){       
     var scroll_start = 0;
     var startchange = $('#startchange');
